01ba468704c022e1349e4853195fbda47ff59b0f
Patti Rameswarappa <PRAMESWA@global.sial.com>
2021-03-24 21:29:11 +0530

    first commit

---
+++ src/screens/Register/Register.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import { Divider, ProfileImage } from '../../components';
+import RightContent from './components/RightContent';
+
+export default class Register extends Component {
+    render() {
+        return (
+            <div className="row">
+                <ProfileImage />
+                <Divider />
+                <RightContent />
+            </div>
+        )
+    }
+}
